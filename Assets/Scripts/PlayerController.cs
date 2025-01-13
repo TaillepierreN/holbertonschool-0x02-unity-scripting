78497246e4a2268020ab300b7e1989daf60544b0
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2025-01-13 11:27:04 +0100

    added trap  behavior

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,10 +3,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed = 5.0f;
-    [SerializeField]  Rigidbody rb;
+    [SerializeField] Rigidbody rb;
 
+    public int health = 5;
     private int score = 0;
-    
+
     void FixedUpdate()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
@@ -16,13 +17,19 @@ public class PlayerController : MonoBehaviour
         rb.AddForce(move * speed);
     }
 
-    private void OnTriggerEnter(Collider other) {
+    private void OnTriggerEnter(Collider other)
+    {
         if (other.gameObject.CompareTag("Pickup"))
-            {
-                score++;
-                Debug.Log($"Score: {score}");
-                other.gameObject.SetActive(false);
-            }
+        {
+            score++;
+            Debug.Log($"Score: {score}");
+            other.gameObject.SetActive(false);
+        }
+        else if (other.gameObject.CompareTag("Trap"))
+        {
+            health--;
+            Debug.Log($"Health: {health}");
+        }
 
     }
 }
